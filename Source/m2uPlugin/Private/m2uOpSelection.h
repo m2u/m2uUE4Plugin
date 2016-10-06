2ac7f0ca25d7af624a899ba5215e7f6cd0ac7ec2
Johannes Becker <alfalfasprossen@gmail.com>
2016-10-06 23:40:45 +0200

    Only notify at the end of a select operation
    `notify` is what takes up the most time during a
    select-operation. Sending notify for each actor sums up quickly, taking
    up to about 2 seconds for ~50 objects. By deferring the notify to after
    all actors have been selected, the actual execution time of the whole
    operation stays in the 'few milliseconds' range.

--- Source/m2uPlugin/Private/m2uOpSelection.h
+++ Source/m2uPlugin/Private/m2uOpSelection.h
@@ -1,16 +1,17 @@
 #pragma once
 // Selection Operations
 
-#include "m2uOperation.h"
-
 #include "ActorEditorUtils.h"
 #include "UnrealEd.h"
+
+#include "m2uOperation.h"
+
 
 class Fm2uOpSelection : public Fm2uOperation
 {
 public:
 
-Fm2uOpSelection( Fm2uOperationManager* Manager = NULL )
+	Fm2uOpSelection(Fm2uOperationManager* Manager=nullptr)
 		:Fm2uOperation( Manager ){}
 
 	bool Execute( FString Cmd, FString& Result ) override
@@ -18,23 +19,28 @@ Fm2uOpSelection( Fm2uOperationManager* Manager = NULL )
 		const TCHAR* Str = *Cmd;
 		bool DidExecute = false;
 
-		if( FParse::Command(&Str, TEXT("SelectByNames")))
+		if (FParse::Command(&Str, TEXT("SelectByNames")))
 		{
-			FString ActorNamesList = FParse::Token(Str,0);
-			TArray<FString> ActorNames = m2uHelper::ParseList(ActorNamesList);			
-			for( FString ActorName : ActorNames )
+			FString ActorNamesList = FParse::Token(Str, 0);
+			TArray<FString> ActorNames = m2uHelper::ParseList(ActorNamesList);
+			for (FString ActorName : ActorNames)
 			{
 				AActor* Actor;
-				if( m2uHelper::GetActorByName(*ActorName, &Actor) )
+				if (m2uHelper::GetActorByName(*ActorName, &Actor))
 				{
-					GEditor->SelectActor( Actor, true, true, true);// actor, select, notify, evenIfHidden
-				}			   
+					GEditor->SelectActor(Actor,
+					                     /*select=*/true,
+					                     /*notify=*/false,
+					                     /*evenIfHidden=*/true);
+				}
 			}
+			GEditor->NoteSelectionChange();
 			GEditor->RedrawLevelEditingViewports();
+
 			DidExecute = true;
 		}
 
-		else if( FParse::Command(&Str, TEXT("DeselectAll")))
+		else if (FParse::Command(&Str, TEXT("DeselectAll")))
 		{
 			GEditor->SelectNone(true, true, false);
 			GEditor->RedrawLevelEditingViewports();
@@ -42,38 +48,37 @@ Fm2uOpSelection( Fm2uOperationManager* Manager = NULL )
 			DidExecute = true;
 		}
 
-		else if( FParse::Command(&Str, TEXT("DeselectByNames")))
+		else if (FParse::Command(&Str, TEXT("DeselectByNames")))
 		{
 			TArray<AActor*> SelectedActors;
 			USelection* Selection = GEditor->GetSelectedActors();
 			Selection->GetSelectedObjects<AActor>(SelectedActors);
-			
-			FString ActorNamesList = FParse::Token(Str,0);
-			TArray<FString> ActorNames = m2uHelper::ParseList(ActorNamesList);			
-			for( FString ActorName : ActorNames )
+
+			FString ActorNamesList = FParse::Token(Str, 0);
+			TArray<FString> ActorNames = m2uHelper::ParseList(ActorNamesList);
+			for (FString ActorName : ActorNames)
 			{
-				for( int32 Idx = 0 ; Idx < SelectedActors.Num() ; ++Idx )
+				for (int32 Idx = 0 ; Idx < SelectedActors.Num() ; ++Idx)
 				{
 					AActor* Actor = SelectedActors[ Idx ];
-					if(Actor->GetFName().ToString() == ActorName)
+					if (Actor->GetFName().ToString() == ActorName)
 					{
 						Selection->Modify();
-						//Selection->BeginBatchSelectOperation();
-						//Selection->Deselect(Actor);
-						//Selection->EndBatchSelectOperation();
-						GEditor->SelectActor( Actor, false, true, true ); // deselect
+						GEditor->SelectActor(Actor,
+						                     /*select=*/false,
+						                     /*notify=*/false,
+						                     /*evenIfHidden=*/true);
 						break;
 					}
-				}	
+				}
 			}
+			GEditor->NoteSelectionChange();
 			GEditor->RedrawLevelEditingViewports();
+
 			DidExecute = true;
 		}
 
 		Result = TEXT("Ok");
-		if( DidExecute )
-			return true;
-		else
-			return false;
+		return DidExecute;
 	}
 };
