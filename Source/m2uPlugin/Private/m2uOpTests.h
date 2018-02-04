1b32f7a7203c30bf528c0ee28650553ac02a9cf8
Johannes Becker <alfalfasprossen@gmail.com>
2018-02-04 15:12:02 +0100

    Add command to setup a specific test scene

--- Source/m2uPlugin/Private/m2uOpTests.h
+++ Source/m2uPlugin/Private/m2uOpTests.h
@@ -29,6 +29,29 @@ public:
 			return true;
 		}
 
+
+		// Initialize a clean scene with specific test data.
+		if (FParse::Command(&Str, TEXT("TestInitializeScene")))
+		{
+			UClass& AssetClass = *UActorFactoryBasicShape::StaticClass();
+			UActorFactory* ActorFactory = GEditor->FindActorFactoryByClass(&AssetClass);
+			UObject* CubeAsset = LoadObject<UStaticMesh>(nullptr,*UActorFactoryBasicShape::BasicCube.ToString());
+			UObject* SphereAsset = LoadObject<UStaticMesh>(nullptr,*UActorFactoryBasicShape::BasicSphere.ToString());
+
+			// GEditor->CreateNewMapForEditing();
+			GUnrealEd->NewMap();
+			ULevel* Level = GEditor->GetEditorWorldContext().World()->GetCurrentLevel();
+			AActor* Cube1 = ActorFactory->CreateActor(CubeAsset, Level, FTransform::Identity, RF_Transactional, FName(TEXT("cube_1")));
+			AActor* Cube2 = ActorFactory->CreateActor(CubeAsset, Level, FTransform::Identity, RF_Transactional, FName(TEXT("cube_2")));
+			Cube2->Rename(TEXT("cube_2"), nullptr, REN_DontCreateRedirectors);
+			AActor* Cube3 = ActorFactory->CreateActor(CubeAsset, Level, FTransform::Identity, RF_Transactional, FName(TEXT("cube_3")));
+			Cube3->Rename(TEXT("cube_3"), nullptr, REN_DontCreateRedirectors);
+			AActor* Sphere1 = ActorFactory->CreateActor(SphereAsset, Level, FTransform::Identity, RF_Transactional, FName(TEXT("sphere_1")));
+
+			Result = FString(TEXT("Initialized"));
+			return true;
+		}
+
 		return false;
 	}
 };
